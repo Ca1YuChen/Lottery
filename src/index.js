7ba6a983d01ea8a4207b4b0409fd2d1ec0fae8c4
yuchen <caiyuchen95@gmail.com>
2017-09-14 02:14:38 +0800

    初始化配置material-ui与sass

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
 registerServiceWorker();
